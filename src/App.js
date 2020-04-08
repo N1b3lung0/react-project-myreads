79fbb4d740d64619c86b2ff366c414d934387614
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-04-08 18:07:22 +0200

    feat: Added a new component, ListBooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import Header from './Header';
+import ListBooks from './ListBooks';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ListBooks />
     </div>
   );
 }
---
+++ src/ListBooks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+
+    render() {
+        return (
+           <div>List Books Component</div> 
+        )
+    }
+}
+
+export default ListBooks
