c410046cb5ad4fcd4bdb31531a445787d4ae83d1
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-04-09 18:26:29 +0200

    feat: Added functionality of search component

--- src/Search.js
+++ src/Search.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react'
 
 class Search extends Component {
 
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
     render() {
+        const { query } = this.state
         return (
-            <div>Search Component</div>
+            <div className="search-books-bar">
+                <input
+                    type='text'
+                    placeholder='Search Books'
+                    value={ query }
+                    onChange={ (event) => this.updateQuery(event.target.value) }
+                />
+                { /* JSON.stringify(query) */ }
+            </div> 
         )
     }
 }
